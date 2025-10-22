9814df26f5d16623fb4d4f6a0e1b1ec559f052e3
julen.guevara <julen.guevara@estudiantat.upc.edu>
2025-10-22 18:39:58 +0200

    feat: test eliminat

--- src/services/cron.service.js
+++ src/services/cron.service.js
@@ -1,7 +1,7 @@
 import cron from 'node-cron';
 import { importNavarraRecyclingPoints } from './navarra.service';
 
-// Programa el cron cada dia a les 03:00 del matí
+//Programa el cron cada dia a les 03:00 del matí
 cron.schedule('0 3 * * *', async () => {
   console.log('🕒 Executant actualització automàtica de punts de reciclatge (Navarra)...');
   await importNavarraRecyclingPoints();
