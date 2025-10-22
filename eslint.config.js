44cda11f1b3b28f074992cb1747cdae6fbe1b507
tacosrra <tacosrra@gmail.com>
2025-10-22 11:56:27 +0200

    feat: lint fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,24 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import { defineConfig } from 'eslint/config';
 
-export default defineConfig([
-  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.node } },
-  tseslint.configs.recommended,
-]);
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+    },
+  },
+  {
+    ignores: ['node_modules/', 'dist/', 'ecomap_data/', '*.config.js'],
+  },
+];
