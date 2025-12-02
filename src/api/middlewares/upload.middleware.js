c89dc7bd991d5cf333ae695af8922ff93a779fb4
arnau.miret <arnau.miret@estudiantat.upc.edu>
2025-12-02 11:15:29 +0100

    feat: linted

--- src/api/middlewares/upload.middleware.js
+++ src/api/middlewares/upload.middleware.js
@@ -7,16 +7,14 @@ const upload = multer({
   fileFilter: (req, file, cb) => {
     const allowedMimeTypes = ['image/jpeg', 'image/png'];
 
-    if(allowedMimeTypes.includes(file.mimetype)) {
+    if (allowedMimeTypes.includes(file.mimetype)) {
       // Aceptar archivo
       cb(null, true);
-    }
-    else {
+    } else {
       // Rechazar archivo
       cb(new Error('INVALID_FILE_TYPE'), false);
-
     }
-  }
+  },
 });
 
 // Para devolver un JSON 'bonito', en lugar de un error HTML o crash del servidor
@@ -24,28 +22,27 @@ export const uploadImageMiddleware = (req, res, next) => {
   const uploadSingle = upload.single('file');
 
   uploadSingle(req, res, (err) => {
-    if(err instanceof multer.MulterError) {
+    if (err instanceof multer.MulterError) {
       // Errores de Multer
-      if(err.code == 'LIMIT_FILE_SIZE') {
+      if (err.code == 'LIMIT_FILE_SIZE') {
         return res.status(400).json({
           success: false,
           message: 'El archivo es demasiado grande. Máximo 5MB.',
-          code: 'FILE_TOO_LARGE'
+          code: 'FILE_TOO_LARGE',
         });
       }
-    }
-    else if(err) {
+    } else if (err) {
       // Errores personalizados
-      if(err.message == 'INVALID_FILE_TYPE') {
+      if (err.message == 'INVALID_FILE_TYPE') {
         return res.status(400).json({
           success: false,
           message: 'Formato de archivo no válido. Solo se permiten JPEG y PNG.',
-          code: 'INVALID_FILE_TYPE'   
+          code: 'INVALID_FILE_TYPE',
         });
       }
       return res.status(500).json({
         success: true,
-        message: 'Error al procesar el archivo.'
+        message: 'Error al procesar el archivo.',
       });
     }
     // Si todo va bien, pasamos al controlador
