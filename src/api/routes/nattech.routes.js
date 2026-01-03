367c931e431b852a1bac8ac7c3524b7e108af1cf
arnau.miret <arnau.miret@estudiantat.upc.edu>
2026-01-03 18:25:48 +0100

    feat: editat nattech.routes

--- src/api/routes/nattech.routes.js
+++ src/api/routes/nattech.routes.js
@@ -4,13 +4,23 @@ import { listEvents, getEvent, createEvent, updateEvent, deleteEvent } from '#co
 
 const router = express.Router();
 
-// rutas públicas
+// ----------------- rutas públicas -----------------
+
+// GET /api/external/events
 router.get('/events', authenticateBackendJWT, listEvents);
+
+// GET /api/external/events/:codi
 router.get('/events/:codi', authenticateBackendJWT, getEvent);
 
-// rutas exclusivas para usuarios 'admin'
+// ----------------- rutas exclusivas para usuarios 'admin' -----------------
+
+// POST /api/external/events
 router.post('/events', authenticateBackendJWT, requireAdmin, createEvent);
+
+// PUT /api/external/events/:codi
 router.put('/events/:codi', authenticateBackendJWT, requireAdmin, updateEvent);
+
+// DELETE /api/external/events/:codi
 router.delete('/events/:codi', authenticateBackendJWT, requireAdmin, deleteEvent);
 
 export default router;
